c515dcc1135485eeaa2788eae27f7f01f3647f0d
Mithilesh <mithileshmarekar01@gmail.com>
2023-08-25 16:14:11 +0530

    done

--- config/database.js
+++ config/database.js
@@ -11,7 +11,7 @@ const db = mongoose.connection; // db store the connection
 // cheacking the connection
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", function () {
-  console.log("DATABASE connection is Established");
+  console.log("Connected to Database");
 });
 
 // exporting the connection.
